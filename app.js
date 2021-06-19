18d343fb67bff916b6a8110f70f43007c24a2dfd
vibheesh <kvibheesh.kumar93@gmail.com>
2021-06-19 23:42:02 +0530

    added Stories

---
+++ app.js
@@ -0,0 +1,80 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const connectDB = require('./config/db')
+
+
+
+dotenv.config({path:'./config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
+
+connectDB()
+
+
+const app = express()
+
+// Body parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+//Logging
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// Handlebars Helpers
+const { formatDate } = require('./helpers/hbs')
+
+
+//Handlebars
+app.engine(
+    '.hbs',
+    exphbs({
+      helpers: {
+        formatDate,
+      },
+      defaultLayout: 'main',
+      extname: '.hbs',
+    })
+  )
+  app.set('view engine', '.hbs')
+
+// Sessions
+app.use(session({ 
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl:"mongodb+srv://asdf1357:asdf1357@cluster0.zt8um.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        
+    })
+
+  }))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
+
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
---
+++ helpers/hbs.js
@@ -0,0 +1,8 @@
+const moment = require('moment')
+
+
+module.exports = {
+    formatData: function (date, format) {
+        return moment(date).format(format)
+    },
+}
